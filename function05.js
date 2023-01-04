3705d9d2ea2b871be653dd49e7dc6ff316ef84a8
pradip baidya <pradipbaidya8888@gmail.com>
2023-01-04 22:57:09 +0530

    making  calculator app

--- function05.js
+++ function05.js
@@ -1,8 +1,68 @@
-// normal Function
+// // normal Function
 
 
-let calculator = {
-     addition(val1,val2){
-        
-    }
-}
+// function calculator = {
+//      addition(val1,val2){
+//         let add = val1 + val2;
+//         return add;
+//     },
+
+//     substraction(val3,val4){
+//         let sub = val3 - val4;
+//         return sub
+//     },
+
+//   multiplication(val5,val6){
+//         let multiply = val5*val6;
+//         return multiply
+//     },
+
+//  divition(val7,val8){
+//         let divi = val7/val8;
+//         return divi
+//     }
+// }
+
+
+// let calculatonAddition = addition(2,2)
+// consoler.log(calculatonAddition);
+
+
+
+// Calculator :
+
+
+function addition(val1,val2){
+    let add = val1 + val2;
+    return add;
+};
+
+function substraction(val3,val4){
+    let sub = val3 - val4;
+    return sub
+};
+
+function multiplication(val5,val6){
+    let multiply = val5*val6;
+    return multiply
+};
+
+function divition(val7,val8){
+    let divi = val7/val8;
+    return divi
+};
+
+
+// Answer :
+
+let calcualtor = addition(10,5);
+console.log(calcualtor);
+
+let calculator1 = substraction(10,5);
+console.log(calculator1);
+
+let calculator2 = multiplication(10,5);
+console.log(calculator2);
+
+let calculator3 = divition(10,5);
+console.log(calculator3);
